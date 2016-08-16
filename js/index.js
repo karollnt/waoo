eee7e1a8a98890e626f73dfd361983fdf878eff5
Rhonalf Martinez <rhonaldomaster@users.noreply.github.com>
2016-08-16 14:37:05 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("deviceready", function(){
 	  window.localStorage.setItem("plataforma", device.platform);
 
-    window.plugins.PushbotsPlugin.initialize("575f09fc4a9efab5a28b4568", {"android":{"sender_id":"306415868516"}});
+    window.plugins.PushbotsPlugin.initialize("575f09fc4a9efab5a28b4568", {"android":{"sender_id":"157120585069"}});
     window.plugins.PushbotsPlugin.on("registered", function(token){
         window.localStorage.setItem("token", token);
     });
