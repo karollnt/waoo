67909f525259a37ee78b7e980fcff07790d1c4a9
rhonaldomaster <rhonalf.martinez@koombea.com>
2017-04-25 16:20:59 -0500

    cambio a plataforma pushwoosh

--- js/index.js
+++ js/index.js
@@ -1,13 +1,86 @@
-document.addEventListener("deviceready", function(){
-	  window.localStorage.setItem("plataforma", device.platform);
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady () {
+  window.localStorage.setItem("plataforma", device.platform);
+  initPushwoosh();
+}
 
-    window.plugins.PushbotsPlugin.initialize("575f09fc4a9efab5a28b4568", {"android":{"sender_id":"157120585069"}});
-    window.plugins.PushbotsPlugin.on("registered", function(token){
-        window.localStorage.setItem("token", token);
-    });
+function onPushwooshInitialized(pushNotification) {
 
-    window.plugins.PushbotsPlugin.getRegistrationId(function(token){
-        window.localStorage.setItem("token", token);
-    });
-}, false);
+  //if you need push token at a later time you can always get it from Pushwoosh plugin
+  pushNotification.getPushToken(function(token) {
+    console.info('push token: ' + token);
+  });
 
+  //and HWID if you want to communicate with Pushwoosh API
+  pushNotification.getPushwooshHWID(function(token) {
+    console.info('Pushwoosh HWID: ' + token);
+  });
+
+  //settings tags
+  pushNotification.setTags({
+    tagName: "tagValue",
+      intTagName: 10
+    },
+    function(status) {
+      console.info('setTags success: ' + JSON.stringify(status));
+    },
+    function(status) {
+      console.warn('setTags failed');
+    }
+  );
+
+  pushNotification.getTags(
+    function(status) {
+      console.info('getTags success: ' + JSON.stringify(status));
+    },
+    function(status) {
+      console.warn('getTags failed');
+    }
+  );
+
+  //start geo tracking.
+  //pushNotification.startLocationTracking();
+}
+
+function initPushwoosh() {
+  var pushwoosh = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+
+  // Should be called before pushwoosh.onDeviceReady
+  document.addEventListener('push-notification', function(event) {
+    var notification = event.notification;
+    // handle push open here
+    // alert(notification.message);
+  });
+
+  // Initialize Pushwoosh. This will trigger all pending push notifications on start.
+  pushwoosh.onDeviceReady({
+    appid: "0E566-1EDAC",
+    projectid: "1021525473859",
+    serviceName: ""
+  });
+
+  if (! (window.localStorage.getItem('isPushwoosh')) ) {
+    window.localStorage.setItem('isPushwoosh', 'true');
+    registerDevice(pushwoosh);
+  }
+  else {
+    if (! (window.localStorage.getItem('token')) ) {
+      registerDevice(pushwoosh);
+    }
+  }
+}
+
+function registerDevice(pushObj) {
+  pushObj.registerDevice(
+    function(status) {
+      var pushToken = status.pushToken;
+      // handle successful registration here
+      window.localStorage.setItem('token', pushToken);
+      onPushwooshInitialized(pushObj);
+    },
+    function(status) {
+      // handle registration error here
+      alert('msg: '+status);
+    }
+  );
+}
