73e0feb566644875eb7fd028db5907cda66dd378
rhonaldomaster <rhonaldomaster@gmail.com>
2017-07-16 17:35:20 -0500

    js para conexion bt

---
+++ js/braintree.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  var ajx = $.ajax({
+    type: "post",
+    url: waooserver+"/usuarios/initBrainTree",
+    dataType: "json",
+    data: {}
+  });
+  ajx.done(function(data) {
+    braintree.dropin.create({
+      authorization: data.msg,
+      container: '.js-dropin-container'
+    }, function (createErr, instance) {
+      var form = document.querySelector('.js-payment-form');
+      form.addEventListener('submit', function (ev) {
+        ev.preventDefault();
+        instance.requestPaymentMethod(function (err, payload) {
+          if (err) {
+            console.log('Error', err);
+            return;
+          }
+          // Add the nonce to the form and submit
+          document.querySelector('#nonce').value = payload.nonce;
+          //form.submit();
+        });
+      });
+    });
+  });
+});
