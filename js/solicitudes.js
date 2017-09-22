665d3d7367e5348f5bd0188363c584faac5a4fee
Karoll Navas <futlaos@gmail.com>
2017-09-22 13:55:35 -0500

    Update solicitudes.js

--- js/solicitudes.js
+++ js/solicitudes.js
@@ -48,62 +48,7 @@ function cargarMateriaSelect(id){
 	});
 }
 
-$(document).ready(function() {
 
-    obtener_datos();
-    $( "#lista").keyup(function() {
-dato = $("#lista").val().trim();
-if (dato.trim().length!=0) {Validar_existe(dato)};
-
-});
-   
-});
-
-    
-function obtener_datos() {
-   
-    $.ajax({
-        url: "http://localhost/Waoo/index.php/Welcome_control/Listar", 
-        dataType: "json",
- 
-        type: "post",
-    }).done(function(datos) {
-      
-      
-     for (var i = 0; i <= datos.length - 1; i++) {
-       
-                $("#lista_tareas").append("<option  value='"+datos[i].nombre+"'>");
-
-            }
-
-             Validar_existe();
-    });
-}
-
-function Validar_existe(dato){
-
-    existe=0;
-var numero = document.getElementById("lista_tareas").options.length
- for (var i = 0; i <= numero - 1; i++) {
-    str  = document.getElementById("lista_tareas").options[i].value;
-    str = str.toLowerCase();
-     var n = str.indexOf(dato.toLowerCase());
-   
-if (n>-1) {
- $("#error").hide("slow")
-existe=1;
-
-return false;
-}
-}
-
-   if (existe==0) {
-   $("#error").show("slow")
-
-   }
-   return false;
-   
-}
 
 
 
